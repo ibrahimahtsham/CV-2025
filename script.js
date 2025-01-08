6220cb14a0a24344c813fd742636086b83bfcee0
Siamax <ibrahimahtsham2002@gmail.com>
2025-01-08 23:13:33 +0500

    more themes

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ let isDarkMode = false;
 
 function toggleStyle() {
   const cvContainer = document.querySelector(".cv-container");
-  cvContainer.classList.remove("style1", "style2", "style3");
+  const headerContainer = document.querySelector(".header-container");
+  const body = document.body;
 
-  currentStyle = (currentStyle + 1) % 3;
+  cvContainer.classList.remove("style1", "style2", "style3", "style4");
+  headerContainer.classList.remove("left-align", "center-align");
+  body.classList.remove("animated-background");
+
+  currentStyle = (currentStyle + 1) % 4;
 
   if (currentStyle === 0) {
     cvContainer.classList.add("style1");
+    headerContainer.classList.add("left-align");
   } else if (currentStyle === 1) {
     cvContainer.classList.add("style2");
+    headerContainer.classList.add("left-align");
   } else if (currentStyle === 2) {
     cvContainer.classList.add("style3");
+    headerContainer.classList.add("center-align");
+  } else if (currentStyle === 3) {
+    cvContainer.classList.add("style4");
+    headerContainer.classList.add("center-align");
+    body.classList.add("animated-background");
   }
 
   toggleTheme();
